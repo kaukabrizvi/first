9e57b48483b66c0fd5b472a6aa6935abf3c682a8
Kaukab Rizvi <zhg4xd@virginia.edu>
2022-03-03 15:26:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
+
+const Header = () => {
+  return <div>This is the Header!</div>
+};
+
+const Bpdy = () => {
+  return <div>
+}
